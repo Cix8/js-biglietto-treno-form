96adc8d40055a6203247f4f5bce4a561afeebb25
Cix8 <francesco.natal_fn@libero.it>
2022-04-08 14:58:49 +0200

    add: ticket-price program in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
+const formBtn = document.getElementById('form-btn');
+console.log(formBtn)
 
+formBtn.addEventListener('click',
+    function () {
+        const userName = document.getElementById('user-name').value;
+        console.log(userName);
+
+        const userKm = parseInt(document.getElementById('user-km').value);
+        console.log(userKm);
+
+        const userAge = document.getElementById('user-age').value;
+        console.log(userAge);
+
+        const priceKm = userKm * 0.21
+        console.log(priceKm);
+
+        let discount;
+
+        if (userAge === 'under18') {
+            discount = (priceKm * 20) / 100;
+        } else if (userAge === 'over65') {
+            discount = (priceKm * 40) / 100;
+        } else {
+            discount = 0;
+        }
+
+        console.log(discount);
+
+        const finalPrice = priceKm - discount
+        console.log(finalPrice);
+        const priceSimple = finalPrice.toFixed(2)
+    }
+)
